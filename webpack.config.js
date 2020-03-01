1ed570addcb1049482074c044b62b5813224db57
Carmen Wick <carmenwick@gmail.com>
2020-03-01 12:28:59 -0800

    Set up Typescript

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        hot: true,
+        compress: true
+    },
+    devtool: 'source-map',
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
