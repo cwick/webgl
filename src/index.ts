024bebe249114f9c8de7b13ff27adc312ab856c6
Carmen Wick <CarmenWick@gmail.com>
2020-03-01 13:39:53 -0800

    Enable eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 class Student {
     fullName: string;
     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
     }
 }
 
@@ -10,10 +10,10 @@ interface Person {
     lastName: string;
 }
 
-function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function greeter(person: Person): string {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
 
-let user = new Student("Jane", "M.", "User");
+const user = new Student('Jane', 'M.', 'User');
 
 document.body.textContent = greeter(user);
