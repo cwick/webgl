00977033686170e7cd1bbcec779354a372ebc386
Carmen Wick <CarmenWick@gmail.com>
2020-03-01 14:04:02 -0800

    WebGL hello world

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,105 @@
-class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+const canvasElement = document.createElement('canvas');
+document.body.append(canvasElement);
+
+const gl = canvasElement.getContext('webgl2');
+if (!gl) {
+    console.error('WebGL 2.0 not available');
+}
+
+const vertexShaderSource = `#version 300 es
+ 
+// an attribute is an input (in) to a vertex shader.
+// It will receive data from a buffer
+in vec4 a_position;
+ 
+// all shaders have a main function
+void main() {
+ 
+  // gl_Position is a special variable a vertex shader
+  // is responsible for setting
+  gl_Position = a_position;
+}
+`;
+
+const fragmentShaderSource = `#version 300 es
+ 
+// fragment shaders don't have a default precision so we need
+// to pick one. mediump is a good default. It means "medium precision"
+precision mediump float;
+ 
+// we need to declare an output for the fragment shader
+out vec4 outColor;
+ 
+void main() {
+  // Just set the output to a constant reddish-purple
+  outColor = vec4(1, 0, 0.5, 1);
+}
+`;
+
+function createShader(gl: WebGL2RenderingContext, type: number, source: string): WebGLShader {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
     }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
 }
 
-interface Person {
-    firstName: string;
-    lastName: string;
+function createProgram(
+    gl: WebGL2RenderingContext,
+    vertexShader: WebGLShader,
+    fragmentShader: WebGLShader,
+): WebGLProgram {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
 }
 
-function greeter(person: Person): string {
-    return 'Hello, ' + person.firstName + ' ' + person.lastName;
-}
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+const program = createProgram(gl, vertexShader, fragmentShader);
+const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-const user = new Student('Jane', 'M.', 'User');
+// three 2d points
+const positions = [0, 0, 0, 0.5, 0.7, 0];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-document.body.textContent = greeter(user);
+const vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+gl.enableVertexAttribArray(positionAttributeLocation);
+
+const size = 2; // 2 components per iteration
+const type = gl.FLOAT; // the data is 32bit floats
+const normalize = false; // don't normalize the data
+const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+const offset = 0; // start at the beginning of the buffer
+gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+// Clear the canvas
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+// Tell it to use our program (pair of shaders)
+gl.useProgram(program);
+
+// Bind the attribute/buffer set we want.
+gl.bindVertexArray(vao);
+
+const primitiveType = gl.TRIANGLES;
+const count = 3;
+gl.drawArrays(primitiveType, offset, count);
