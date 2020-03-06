d436497d816124bf9fb5421f7cf367e92e637e30
Carmen Wick <CarmenWick@gmail.com>
2020-03-05 20:57:44 -0800

    Load buffers from gltf file

--- src/GLTFLoader.ts
+++ src/GLTFLoader.ts
@@ -2,14 +2,33 @@ import example from 'assets/example.gltf';
 
 export default class GLTFLoader {
     load(): void {
-        // this.getRequiredPath('asset.version');
+        const version = example.asset.version;
+        if (!version.startsWith('2.')) {
+            this.notSupported(`glTF version ${version} not supported.`);
+        }
+
+        if (!example.meshes?.[0].primitives.length) {
+            return;
+        }
+
+        example.buffers?.forEach(async (buffer, i) => {
+            if (!buffer.uri) {
+                this.invalid(`missing buffer uri for buffer ${i}`);
+            }
+            console.log(await this.loadBufferFromURI(buffer.uri));
+        });
     }
-    // private getRequiredPath(path: string): [object | number] {
-    //     const components = path.split('.');
-    //     let obj = example;
-    //     for (let i = 0; i < components.length; i++) {
-    //         if
-    //         obj = obj[components[i]];
-    //     }
-    // }
+
+    private async loadBufferFromURI(uri: string): Promise<ArrayBuffer> {
+        const response = await fetch(uri);
+        return await response.arrayBuffer();
+    }
+
+    private notSupported(message: string): never {
+        throw new Error(message);
+    }
+
+    private invalid(message: string): never {
+        throw new Error(`Invalid glTF file: ${message}`);
+    }
 }
